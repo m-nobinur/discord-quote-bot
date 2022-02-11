cef9c802ac972c49eaf2407a01dc7fc9f589f061
Nobin <nobinkhan.me@gmail.com>
2022-02-11 19:27:58 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Discord Quote Bot 🤖
+
+A discord bot that send random quote on command to the Channel.
---
+++ quote-bot.py
@@ -0,0 +1,12 @@
+"""
+Name: discord-quote-bot
+Description: This is a template to create your own discord bot in python.
+Author: Mohammad Nobinur
+
+Using https://github.com/DisnakeDev/disnake
+API: http://disnake.readthedocs.io/en/latest/api.html
+
+Copyright © m-nobinur 2022 - https://github.com/m-nobinur
+
+"""
+
